61fd30d9c789b18a845df86d5c47fa9476f258f5
Diana Sahan <sahandianaa@gmail.com>
2023-11-26 06:30:33 -0800

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 import { QuizesList } from './pages/quizesList/QuizesList';
+import { Quiz } from './components/quiz/Quiz';
 
 import styles from './App.scss';
 
 function App() {
   return (
     <div className={styles.app}>
-      <QuizesList />
+      <Routes>
+        <Route path="/" exact Component={QuizesList} />
+        <Route path="/:id" Component={Quiz} />
+      </Routes>
     </div>
   );
 }
