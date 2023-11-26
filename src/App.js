2bed250016a3269f8c88bc834ba987be4735907d
Diana Sahan <sahandianaa@gmail.com>
2023-11-25 16:54:42 -0800

    Init project with create-react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import { Quiz } from './features/quiz/Quiz';
+
+import styles from './App.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <header className={styles.header}>
+        {/* <Quiz /> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
